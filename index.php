f328063e464ea11ecdecd42aa12061a6ca072482
KatarinaSimonoviccc <133761613+KatarinaSimonoviccc@users.noreply.github.com>
2023-05-16 15:02:25 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Moja index stranica</title>
+</head>
+<body>
+    <h1>Dobrodošli na moju index stranicu</h1>
+
+    <?php
+    // Ovdje možete dodati PHP kod za obradu ili prikazivanje podataka
+    ?>
+
+    <p>Ovo je primjer osnovne index stranice u PHP-u.</p>
+</body>
+</html>
